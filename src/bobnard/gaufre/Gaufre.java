f95f4d9f6dcc82c4fec8ae91fd8ede11fae24a30
maxbd <maxbda74@gmail.com>
2021-05-06 15:16:07 +0200

    Test

--- src/bobnard/gaufre/Gaufre.java
+++ src/bobnard/gaufre/Gaufre.java
@@ -4,6 +4,6 @@ import bobnard.gaufre.UI.MainWindow;
 
 public class Gaufre {
     public static void main(String[] args) {
-        MainWindow.start();
+        MainWindow.start(); // Test
     }
 }
