d5e017bc4075f1b990565ce2e79f4a76c1125975
Esdia <theo.hermitte@protonmail.com>
2021-05-08 01:25:51 +0200

    Rewrote the listener to use the new controller

--- src/bobnard/gaufre/UI/Mouse.java
+++ src/bobnard/gaufre/UI/Mouse.java
@@ -1,28 +1,17 @@
 package bobnard.gaufre.UI;
 
-import bobnard.gaufre.model.Game;
-
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Mouse extends MouseAdapter {
-    GaufreUI gaufreUI;
-    Game game;
+    GameController controller;
 
-    public Mouse(GaufreUI gaufreUI, Game game) {
-        this.gaufreUI = gaufreUI;
-        this.game = game;
+    public Mouse(GameController controller) {
+        this.controller = controller;
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-        // TODO Mouse click
-
-        int x = (e.getX() - this.gaufreUI.getStartX()) / this.gaufreUI.getTileSize();
-        int y = (e.getY() - this.gaufreUI.getStartY()) / this.gaufreUI.getTileSize();
-
-        this.game.turn(x, y);
-
-        this.gaufreUI.repaint();
+        this.controller.interpretClick(e.getX(), e.getY());
     }
 }
