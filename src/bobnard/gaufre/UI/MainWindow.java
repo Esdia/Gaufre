5f7f10bb4c999810f57656323b2ce9c19459d474
Esdia <theo.hermitte@protonmail.com>
2021-05-07 15:58:34 +0200

    Moved some things around

--- src/bobnard/gaufre/UI/MainWindow.java
+++ src/bobnard/gaufre/UI/MainWindow.java
@@ -1,34 +1,65 @@
 package bobnard.gaufre.UI;
 
 import bobnard.gaufre.model.Game;
-import bobnard.gaufre.model.Grid;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class MainWindow implements Runnable {
-    @Override
-    public void run() {
-        JFrame frame = new JFrame("Gaufre empoisonnée");
-        Game game = new Game(5,5);
+    Game game;
+    GaufreUI gaufreUI;
+    JFrame frame;
 
-        GaufreUI gaufreUI = new GaufreUI(game);
+    private void createGame() {
+        this.game = new Game(5, 5);
+    }
 
-        gaufreUI.addMouseListener(new Mouse());
-        frame.add(gaufreUI);
+    private void createGaufreUI() {
+        this.gaufreUI = new GaufreUI(this.game);
+        this.gaufreUI.addMouseListener(new Mouse(this.gaufreUI, this.game));
+    }
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private void createFrame() {
+        this.frame = new JFrame("Gaufre empoisonnée");
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        frame.setSize(
+        this.frame.setSize(
                 screenSize.width * 3 / 4,
                 screenSize.height * 3 / 4
         );
-        frame.setLocation(
-                (screenSize.width - frame.getWidth()) / 2,
-                (screenSize.height - frame.getHeight()) / 2
+        this.frame.setLocationRelativeTo(null);
+
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Box menuBox = Box.createVerticalBox();
+        menuBox.setSize(this.frame.getWidth() / 4, this.frame.getHeight());
+        menuBox.setBackground(Color.CYAN);
+
+        this.frame.addComponentListener(
+                new ComponentAdapter() {
+                    @Override
+                    public void componentResized(ComponentEvent e) {
+                        super.componentResized(e);
+                        gaufreUI.setSize(frame.getContentPane().getWidth() * 3 / 4, frame.getContentPane().getHeight());
+                    }
+                }
         );
+
+
+        this.frame.add(this.gaufreUI);
+        this.frame.add(menuBox);
+
+
         frame.setVisible(true);
+    }
+
+    @Override
+    public void run() {
+        this.createGame();
+        this.createGaufreUI();
+        this.createFrame();
     }
 
     public static void start() {
