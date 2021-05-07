3b57c6b5148b8853f063533b38f1d014fd5a5bd0
Esdia <theo.hermitte@protonmail.com>
2021-05-08 01:28:30 +0200

    Rewrote the UI to use the new labels and buttons

--- src/bobnard/gaufre/UI/MainWindow.java
+++ src/bobnard/gaufre/UI/MainWindow.java
@@ -1,66 +1,146 @@
 package bobnard.gaufre.UI;
 
+import bobnard.gaufre.UI.buttons.*;
+import bobnard.gaufre.UI.labels.CustomLabel;
+import bobnard.gaufre.UI.labels.LabelScore;
+import bobnard.gaufre.UI.labels.LabelTurn;
 import bobnard.gaufre.model.Game;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
+import java.util.ArrayList;
 
 public class MainWindow implements Runnable {
-    Game game;
-    GaufreUI gaufreUI;
+    GameController controller;
     JFrame frame;
 
-    private void createGame() {
-        this.game = new Game(5, 5);
+    private void initGameController() {
+        Game game = new Game(5, 10);
+        GaufreUI gaufreUI = new GaufreUI(game);
+
+        ArrayList<Updatable> updatables = new ArrayList<>();
+
+        this.controller = new GameController(
+                gaufreUI,
+                game,
+                updatables
+        );
     }
 
-    private void createGaufreUI() {
-        this.gaufreUI = new GaufreUI(this.game);
-        this.gaufreUI.addMouseListener(new Mouse(this.gaufreUI, this.game));
+    private ArrayList<CustomLabel> createLabels() {
+        ArrayList<CustomLabel> labels = new ArrayList<>();
+
+        labels.add(new LabelTurn(controller.getGame()));
+        labels.add(new LabelScore(controller.getGame()));
+
+        return labels;
     }
 
-    private void createFrame() {
+    private JPanel createUndoRedoPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(1, 0, 20, 20));
+        panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        ButtonRedo redo = new ButtonRedo(controller);
+        ButtonUndo undo = new ButtonUndo(controller);
+
+        controller.addUpdatable(redo);
+        controller.addUpdatable(undo);
+
+        panel.add(redo);
+        panel.add(undo);
+
+        return panel;
+    }
+
+    private JPanel createSaveLoadPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(1, 0, 20, 20));
+        panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        ButtonSave save = new ButtonSave(controller);
+        ButtonLoad load = new ButtonLoad(controller);
+
+        panel.add(save);
+        panel.add(load);
+
+        return panel;
+    }
+
+    private JPanel createButtonPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(0, 1, 20, 20));
+        panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        panel.add(new ButtonNewGame(controller));
+        panel.add(new ButtonResetScores(controller));
+
+        panel.add(this.createUndoRedoPanel());
+        panel.add(this.createSaveLoadPanel());
+
+        panel.add(new ButtonQuit(controller));
+
+        return panel;
+    }
+
+    private JPanel createMainPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBorder(new EmptyBorder(0, 30, 0 ,30));
+
+        /* Space before the rest */
+        panel.add(Box.createGlue());
+
+        /* Labels */
+        for (CustomLabel label: this.createLabels()) {
+            controller.addUpdatable(label);
+            panel.add(label);
+        }
+
+        /* Buttons */
+        panel.add(this.createButtonPanel());
+
+        panel.add(Box.createGlue());
+
+        return panel;
+    }
+
+    private void createBaseFrame() {
         this.frame = new JFrame("Gaufre empoisonnée");
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        this.frame.setMinimumSize(new Dimension(480, 360));
-        this.frame.setSize(
+        frame.setMinimumSize(new Dimension(480, 360));
+        frame.setSize(
                 screenSize.width * 3 / 4,
                 screenSize.height * 3 / 4
         );
-        this.frame.setLocationRelativeTo(null);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
-        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private void createFrame() {
+        this.createBaseFrame();
 
-        Box menuBox = Box.createVerticalBox();
-        menuBox.setSize(this.frame.getWidth() / 4, this.frame.getHeight());
-        menuBox.setBackground(Color.CYAN);
+        this.frame.add(controller.getGaufreUI());
+        this.frame.add(this.createMainPanel(), BorderLayout.EAST);
 
-        this.frame.addComponentListener(
-                new ComponentAdapter() {
-                    @Override
-                    public void componentResized(ComponentEvent e) {
-                        super.componentResized(e);
-                        gaufreUI.setSize(frame.getContentPane().getWidth() * 3 / 4, frame.getContentPane().getHeight());
-                    }
-                }
-        );
+        this.frame.setVisible(true);
+    }
 
+    private void linkMouseListener() {
+        Mouse mouse = new Mouse(controller);
+        GaufreUI gaufreUI = controller.getGaufreUI();
 
-        this.frame.add(this.gaufreUI);
-        this.frame.add(menuBox);
-
-
-        frame.setVisible(true);
+        gaufreUI.addMouseListener(mouse);
     }
 
     @Override
     public void run() {
-        this.createGame();
-        this.createGaufreUI();
+        this.initGameController();
+
         this.createFrame();
+        this.linkMouseListener();
     }
 
     public static void start() {
