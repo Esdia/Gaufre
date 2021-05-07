5e897d5f534a2cb2a82a8461666ab396c6a44a0c
Esdia <theo.hermitte@protonmail.com>
2021-05-07 15:08:08 +0200

    Added the UI and the game to the mouse listener

--- src/bobnard/gaufre/UI/Mouse.java
+++ src/bobnard/gaufre/UI/Mouse.java
@@ -1,9 +1,19 @@
 package bobnard.gaufre.UI;
 
+import bobnard.gaufre.model.Game;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Mouse extends MouseAdapter {
+    GaufreUI gaufreUI;
+    Game game;
+
+    public Mouse(GaufreUI gaufreUI, Game game) {
+        this.gaufreUI = gaufreUI;
+        this.game = game;
+    }
+
     @Override
     public void mousePressed(MouseEvent e) {
         // TODO Mouse click
